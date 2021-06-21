008cb53df7b0428a72b40720555936adce470f88
Andrew <20448589@life.hkbu.edu.hk>
2021-06-21 10:03:01 +0800

    a

---
+++ twoDarray.java
@@ -0,0 +1,7 @@
+public class twoDarray{
+    public static void main(String [] args){
+        int[][] myNumbers = {{1,2,3,4}, {5,6,7,8}};
+        int x = myNumbers[1][1];
+        System.out.println(x);
+    }
+}
